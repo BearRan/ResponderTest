1601c0a6db96608ca1d453c8fd549b4e948b83ec
BearXR <648070256@qq.com>
2020-06-16 23:20:24 +0800

    study

--- ResponderTest/BaseView.swift
+++ ResponderTest/BaseView.swift
@@ -29,22 +29,26 @@ class BaseView: UIView {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
+        
         print("--touchesBegan name:\(self.name)")
+        super.touchesBegan(touches, with: event)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesMoved(touches, with: event)
+        
         print("--touchesMoved name:\(self.name)")
+        super.touchesMoved(touches, with: event)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesEnded(touches, with: event)
+        
         print("--touchesEnded name:\(self.name)")
+        super.touchesEnded(touches, with: event)
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesCancelled(touches, with: event)
+        
         print("--touchesCancelled name:\(self.name)")
+        super.touchesCancelled(touches, with: event)
     }
 }
--- ResponderTest/TestGestureVC.swift
+++ ResponderTest/TestGestureVC.swift
@@ -13,6 +13,9 @@ class TestGestureVC: BaseViewController {
     let viewA = BaseView(name: "viewA")
     let viewB = BaseView(name: "viewB")
     
+    let viewC = BaseView(name: "viewC")
+    let viewD = BaseView(name: "viewD")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,12 +24,22 @@ class TestGestureVC: BaseViewController {
     
     // MARK: - Create UI
     fileprivate func createUI() {
+        createTestViewHaveGesture()
+        createTestViewNoGesture()
+    }
+    
+    func createTestViewHaveGesture() {
         self.view.addSubview(viewA)
         viewA.backgroundColor = .orange
         viewA.snp.makeConstraints { (make) in
             make.width.height.equalTo(200)
-            make.centerX.centerY.equalToSuperview()
+            make.top.equalToSuperview().offset(100)
+            make.centerX.equalToSuperview()
         }
+        
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapEvent))
+        self.viewA.addGestureRecognizer(tapGR)
+        
         
         self.viewA.addSubview(self.viewB)
         viewB.backgroundColor = .blue
@@ -34,5 +47,44 @@ class TestGestureVC: BaseViewController {
             make.width.height.equalTo(100)
             make.centerX.centerY.equalToSuperview()
         }
+        
+        let label = UILabel()
+        label.text = "have gesture"
+        label.textColor = .white
+        self.viewB.addSubview(label)
+        label.snp.makeConstraints { (make) in
+            make.centerX.centerY.equalToSuperview()
+        }
+        
+    }
+    
+    func createTestViewNoGesture() {
+        self.view.addSubview(viewC)
+        viewC.backgroundColor = .orange
+        viewC.snp.makeConstraints { (make) in
+            make.width.height.equalTo(200)
+            make.top.equalTo(self.viewA.snp.bottom).offset(100)
+            make.centerX.equalToSuperview()
+        }
+        
+        
+        self.viewC.addSubview(self.viewD)
+        viewD.backgroundColor = .blue
+        viewD.snp.makeConstraints { (make) in
+            make.width.height.equalTo(100)
+            make.centerX.centerY.equalToSuperview()
+        }
+        
+        let label = UILabel()
+        label.text = "no gesture"
+        label.textColor = .white
+        self.viewD.addSubview(label)
+        label.snp.makeConstraints { (make) in
+            make.centerX.centerY.equalToSuperview()
+        }
+    }
+    
+    @objc func tapEvent() {
+        print("--tapEvent")
     }
 }
