8afb2d6ff7c4064f6df8844f68c80b2e897d2839
BearXR <648070256@qq.com>
2020-06-16 22:45:50 +0800

    study

---
+++ ResponderTest/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  ResponderTest
+//
+//  Created by Chobits on 2020/6/16.
+//  Copyright © 2020 Chobits. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ResponderTest/ViewController.swift
+++ ResponderTest/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    lazy var vcNames = [
+        "TestGestureVC",
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
     }
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--select name:\(vcNames[indexPath.row])")
+        
+        let calssName = vcNames[indexPath.row]
+        let namespace = Bundle.main.infoDictionary!["CFBundleExecutable"] as! String
+        if let tmpClass = NSClassFromString("\(namespace).\(calssName)") as? UIViewController.Type {
+            let tmpVC = tmpClass.init()
+            self.navigationController?.pushViewController(tmpVC, animated: true)
+        }
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vcNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellId = "cellId"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellId)
+        if cell == nil {
+            cell = UITableViewCell.init(style: .default, reuseIdentifier: cellId)
+        }
+        cell?.textLabel?.text = vcNames[indexPath.row]
+        
+        return cell!
+    }
 }
 
