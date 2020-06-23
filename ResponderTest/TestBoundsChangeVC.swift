31c80bc6245936aadcfb9a80e53430926946e3d2
BearXR <648070256@qq.com>
2020-06-23 22:34:06 +0800

    增加测试代码

--- ResponderTest/TestBoundsChangeVC.swift
+++ ResponderTest/TestBoundsChangeVC.swift
@@ -16,12 +16,13 @@ class TestBoundsChangeVC: BaseViewController {
         testA()
         testB()
         testC()
+        testD()
     }
     
     func testA() {
         let contentTestView = BaseContentView()
         self.stackView.addArrangedSubview(contentTestView)
-        contentTestView.name = "bounds未修改"
+        
         
         let viewA = UIView(frame: CGRect.init(x: 50, y: 50, width: 100, height: 100))
         viewA.backgroundColor = .orange
@@ -30,12 +31,13 @@ class TestBoundsChangeVC: BaseViewController {
         let viewB = UIView(frame: CGRect.init(x: 0, y: 0, width: 50, height: 50))
         viewB.backgroundColor = .yellow
         viewA.addSubview(viewB)
+        
+        contentTestView.name = "bounds未修改, viewB.center\(viewB.center)"
     }
     
     func testB() {
         let contentTestView = BaseContentView()
         self.stackView.addArrangedSubview(contentTestView)
-        contentTestView.name = "bounds修改后 viewA.bounds = CGRect.init(x: -25, y: -25, width: 100, height: 100)"
         
         let viewA = UIView(frame: CGRect.init(x: 50, y: 50, width: 100, height: 100))
         viewA.backgroundColor = .orange
@@ -46,12 +48,13 @@ class TestBoundsChangeVC: BaseViewController {
         let viewB = UIView(frame: CGRect.init(x: 0, y: 0, width: 50, height: 50))
         viewB.backgroundColor = .yellow
         viewA.addSubview(viewB)
+        
+        contentTestView.name = "bounds修改后 viewA.bounds = CGRect.init(x: -25, y: -25, width: 100, height: 100), viewB.center\(viewB.center)"
     }
     
     func testC() {
         let contentTestView = BaseContentView()
         self.stackView.addArrangedSubview(contentTestView)
-        contentTestView.name = "bounds修改后 viewA.bounds = CGRect.init(x: 25, y: 25, width: 100, height: 100)"
         
         let viewA = UIView(frame: CGRect.init(x: 50, y: 50, width: 100, height: 100))
         viewA.backgroundColor = .orange
@@ -62,6 +65,26 @@ class TestBoundsChangeVC: BaseViewController {
         let viewB = UIView(frame: CGRect.init(x: 0, y: 0, width: 50, height: 50))
         viewB.backgroundColor = .yellow
         viewA.addSubview(viewB)
+        
+        contentTestView.name = "bounds修改后 viewA.bounds = CGRect.init(x: 25, y: 25, width: 100, height: 100), viewB.center\(viewB.center)"
+    }
+    
+    func testD() {
+        let contentTestView = BaseContentView()
+        self.stackView.addArrangedSubview(contentTestView)
+        
+        let viewA = UIView(frame: CGRect.init(x: 50, y: 50, width: 100, height: 100))
+        viewA.backgroundColor = .orange
+        contentTestView.addSubview(viewA)
+        
+        let viewB = UIView(frame: CGRect.init(x: 0, y: 0, width: 50, height: 50))
+        viewB.backgroundColor = .yellow
+        viewA.addSubview(viewB)
+        
+        viewA.bounds = CGRect.init(x: -50, y: -50, width: 100, height: 100)
+        viewB.center = CGPoint.init(x: 0, y: 0)
+        
+        contentTestView.name = "bounds修改后 viewA.bounds = CGRect.init(x: -50, y: -50, width: 100, height: 100), viewB.center\(viewB.center)"
     }
 
 }
